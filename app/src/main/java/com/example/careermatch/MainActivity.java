6ecbab8c43be9c570e83d5e516aad871198f8671
oledibefrancis <somtofrancis380@gmail.com>
2022-06-25 14:52:44 -0700

    Implemented switching between different tabs using using fragments and a Bottom Navigation View

--- app/src/main/java/com/example/careermatch/MainActivity.java
+++ app/src/main/java/com/example/careermatch/MainActivity.java
@@ -2,17 +2,24 @@ package com.example.careermatch;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.example.careermatch.fragments.HomeFragment;
+import com.example.careermatch.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigation;
+    final FragmentManager fragmentManager = getSupportFragmentManager()  ;
+    FrameLayout flContainer;
 
 
     @Override
@@ -21,25 +28,32 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         bottomNavigation = findViewById(R.id.bottomNavigation);
+        flContainer = findViewById(R.id.flContainer);
 
         bottomNavigation.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
             @Override
             public void onNavigationItemReselected(@NonNull MenuItem item) {
+                Fragment fragment;
                 switch (item.getItemId()) {
                     case R.id.action_home:
                         Toast.makeText(MainActivity.this,"Home",Toast.LENGTH_SHORT).show();
+                        fragment = new HomeFragment();
                         break;
                     case R.id.action_compose:
                         Toast.makeText(MainActivity.this,"Roadmap",Toast.LENGTH_SHORT).show();
+                        fragment = new HomeFragment();
                         break;
                     case R.id.action_profile:
                     default:
                         Toast.makeText(MainActivity.this,"Profile",Toast.LENGTH_SHORT).show();
+                        fragment = new ProfileFragment();
                         break;
                 }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
             }
         });
         bottomNavigation.setSelectedItemId(R.id.action_home);
-
     }
+    //TODO 1) add filter home view, add search, add log out button
+
 }
--- app/src/main/java/com/example/careermatch/fragments/HomeFragment.java
+++ app/src/main/java/com/example/careermatch/fragments/HomeFragment.java
@@ -5,12 +5,33 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.careermatch.Book;
+import com.example.careermatch.BookAdapter;
+import com.example.careermatch.BookClient;
 import com.example.careermatch.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,11 +39,14 @@ import com.example.careermatch.R;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
-
+    public static final String GOOGLE_BOOK_URL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=66ed036507b74261af45f98c30aa8f69";
+    List<Book> books;
+    RecyclerView rvHomeFeeds;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "HomeFragment";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -69,6 +93,32 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        rvHomeFeeds = view.findViewById(R.id.rvHomeFeeds);
 
-    }
-}
+        books = new ArrayList<>();
+        BookAdapter bookAdapter = new BookAdapter(getContext(), books);
+        rvHomeFeeds.setAdapter(bookAdapter);
+        rvHomeFeeds.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(GOOGLE_BOOK_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess: " + json.toString());
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    books.addAll(Book.fromJsonArray(results));
+                    bookAdapter.notifyDataSetChanged();
+                    Log.i(TAG,"Results:"+ results.toString());
+                } catch (JSONException e) {
+                    Log.e("HomeFragment", "Error: " + e.toString());
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
+
+}}
