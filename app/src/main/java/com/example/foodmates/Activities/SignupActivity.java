191cddfe6e273c511146e562659ad9a5f58ef322
oledibefrancis <somtofrancis380@gmail.com>
2022-07-11 12:21:28 -0700

    Implemented Date Picker
    This allows the user to pick a date through a calendar that pops up.

--- app/src/main/java/com/example/foodmates/Activities/SignupActivity.java
+++ app/src/main/java/com/example/foodmates/Activities/SignupActivity.java
@@ -1,21 +1,31 @@
 package com.example.foodmates.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.foodmates.R;
+import com.example.foodmates.fragments.ChatFragment;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
-public class SignupActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class SignupActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
 
     public static final String TAG = "SignUpActivity";
 
@@ -23,9 +33,10 @@ public class SignupActivity extends AppCompatActivity {
     EditText etPassword;
     EditText etEmail;
     EditText  etContact;
-    EditText etBirthDate;
+    TextView etBirthDate;
     Button etSignUpButton;
     Button loginButton;
+    ImageView iconDate;
 
 
 
@@ -40,6 +51,8 @@ public class SignupActivity extends AppCompatActivity {
         etEmail = findViewById(R.id.etEmail);
         etContact = findViewById(R.id.etContact);
         etBirthDate = findViewById(R.id.etBirthDate);
+        iconDate = findViewById(R.id.iconDate);
+
 
 
 
@@ -64,7 +77,12 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
-
+        iconDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              showDatePickerDialog();
+            }
+        });
 
     }
 
@@ -102,4 +120,25 @@ public class SignupActivity extends AppCompatActivity {
         );
     }
 
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        String currentDateString = DateFormat.getDateInstance().format(calendar.getTime());
+        etBirthDate.setText(currentDateString);
+    }
+    private void showDatePickerDialog(){
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+                );
+        datePickerDialog.show();
+    }
 }
