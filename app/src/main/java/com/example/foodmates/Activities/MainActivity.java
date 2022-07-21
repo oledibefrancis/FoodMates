4790edbbdef41bc03f9d2f2ce9403f5507f07eec
oledibefrancis <somtofrancis380@gmail.com>
2022-07-21 12:20:52 -0700

    Alert dialog to confirm if the user wants to exit the app

--- app/src/main/java/com/example/foodmates/Activities/MainActivity.java
+++ app/src/main/java/com/example/foodmates/Activities/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.app.AlertDialog;
+import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
@@ -16,13 +19,9 @@ import com.example.foodmates.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    final FragmentManager fragmentManager = getSupportFragmentManager();
     BottomNavigationView bottomNavigation;
-    final FragmentManager fragmentManager = getSupportFragmentManager()  ;
     FrameLayout flContainer;
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,5 +53,17 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigation.setSelectedItemId(R.id.action_home);
     }
 
-
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you really want to exit?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        MainActivity.super.onBackPressed();
+                    }
+                }).setNegativeButton("No", null);
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
 }
