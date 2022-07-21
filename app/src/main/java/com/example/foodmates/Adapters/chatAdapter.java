72b04acafcb3d2bea7a18fec92551b190c166665
oledibefrancis <somtofrancis380@gmail.com>
2022-07-21 12:19:21 -0700

    The user layout shows an avatar with the message if the user hasn't uploaded a profile picture

--- app/src/main/java/com/example/foodmates/Adapters/chatAdapter.java
+++ app/src/main/java/com/example/foodmates/Adapters/chatAdapter.java
@@ -1,6 +1,7 @@
 package com.example.foodmates.Adapters;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,23 +12,42 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.example.foodmates.Models.Chat;
 import com.example.foodmates.Models.Message;
 import com.example.foodmates.R;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
-import com.parse.ParseRelation;
+import com.parse.ParseUser;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.List;
 
-public class chatAdapter extends RecyclerView.Adapter<chatAdapter.MessageViewHolder>{
+public class chatAdapter extends RecyclerView.Adapter<chatAdapter.MessageViewHolder> {
+    public static final String TAG = "chatAdapter";
     private static final int MESSAGE_OUTGOING = 123;
     private static final int MESSAGE_INCOMING = 321;
-
     private List<Message> mMessages;
     private Context mContext;
     private String mUserId;
+
+    public chatAdapter(Context context, String userId, List<Message> messages) {
+        this.mUserId = userId;
+        mMessages = messages;
+        mContext = context;
+    }
+
+    private static String getProfileUrl(final String userId) {
+        String hex = "";
+        try {
+            final MessageDigest digest = MessageDigest.getInstance("MD5");
+            final byte[] hash = digest.digest(userId.getBytes());
+            final BigInteger bigInt = new BigInteger(hash);
+            hex = bigInt.abs().toString(16);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "https://www.gravatar.com/avatar/" + hex + "?d=identicon";
+    }
 
     @Override
     public int getItemViewType(int position) {
@@ -41,23 +61,6 @@ public class chatAdapter extends RecyclerView.Adapter<chatAdapter.MessageViewHol
     private boolean isUser(int position) {
         Message message = mMessages.get(position);
         return message.getUserId() != null && message.getUserId().equals(mUserId);
-    }
-    private static String getProfileUrl(final String userId) {
-        String hex = "";
-        try {
-            final MessageDigest digest = MessageDigest.getInstance("MD5");
-            final byte[] hash = digest.digest(userId.getBytes());
-            final BigInteger bigInt = new BigInteger(hash);
-            hex = bigInt.abs().toString(16);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "https://www.gravatar.com/avatar/" + hex + "?d=identicon";
-    }
-    public chatAdapter(Context context, String userId, List<Message> messages) {
-        this.mUserId = userId;
-        mMessages = messages;
-        mContext = context;
     }
 
     @NonNull
@@ -87,11 +90,12 @@ public class chatAdapter extends RecyclerView.Adapter<chatAdapter.MessageViewHol
         return mMessages.size();
     }
 
-    public abstract class MessageViewHolder extends RecyclerView.ViewHolder{
+    public abstract class MessageViewHolder extends RecyclerView.ViewHolder {
 
         public MessageViewHolder(@NonNull View itemView) {
             super(itemView);
         }
+
         abstract void bindMessage(Message message);
     }
 
@@ -102,19 +106,28 @@ public class chatAdapter extends RecyclerView.Adapter<chatAdapter.MessageViewHol
 
         public IncomingMessageViewHolder(View itemView) {
             super(itemView);
-            imageOther = (ImageView)itemView.findViewById(R.id.ivProfileOther);
-            body = (TextView)itemView.findViewById(R.id.tvBody);
-            name = (TextView)itemView.findViewById(R.id.tvName);
+            imageOther = (ImageView) itemView.findViewById(R.id.ivProfileOther);
+            body = (TextView) itemView.findViewById(R.id.tvBody);
+            name = (TextView) itemView.findViewById(R.id.tvName);
         }
 
         @Override
         public void bindMessage(Message message) {
-            Glide.with(mContext)
-                    .load(getProfileUrl(message.getUserId()))
-                    .circleCrop()
-                    .into(imageOther);
             body.setText(message.getBody());
-            name.setText(message.getUserId());
+            name.setText(message.getUserIdPointer().getUsername());
+            ParseFile profileImg = message.getUserIdPointer().getParseFile("profilePicture");
+            if (profileImg == null) {
+                Glide.with(mContext)
+                        .load(getProfileUrl(message.getUserId()))
+                        .circleCrop()
+                        .into(imageOther);
+            } else {
+                Glide.with(mContext)
+                        .load(profileImg.getUrl())
+                        .circleCrop()
+                        .into(imageOther);
+            }
+
         }
     }
 
@@ -124,17 +137,23 @@ public class chatAdapter extends RecyclerView.Adapter<chatAdapter.MessageViewHol
 
         public OutgoingMessageViewHolder(View itemView) {
             super(itemView);
-            imageMe = (ImageView)itemView.findViewById(R.id.ivProfileMe);
-            body = (TextView)itemView.findViewById(R.id.tvBody);
+            imageMe = (ImageView) itemView.findViewById(R.id.ivProfileMe);
+            body = (TextView) itemView.findViewById(R.id.tvBody);
         }
 
         @Override
         public void bindMessage(Message message) {
-            Glide.with(mContext)
-                    .load(getProfileUrl(message.getUserId()))
-                    .circleCrop()
-                    .into(imageMe);
-            body.setText(message.getBody());        }
+            body.setText(message.getBody());
+            if (ParseUser.getCurrentUser().getParseFile("profilePicture") == null) {
+                Glide.with(mContext).load(getProfileUrl(ParseUser.getCurrentUser().getObjectId())).circleCrop().into(imageMe);
+            } else {
+                Glide.with(mContext)
+                        .load(ParseUser.getCurrentUser().getParseFile("profilePicture").getUrl())
+                        .circleCrop()
+                        .into(imageMe);
+            }
+        }
     }
-
 }
+
+
