a2fc264dfc08a10e1de30fbcc3617826f5d95f9f
oledibefrancis <somtofrancis380@gmail.com>
2022-07-21 12:27:42 -0700

    Show recipe detail in the detail activity using

--- app/src/main/java/com/example/foodmates/Activities/PostDetailsActivity.java
+++ app/src/main/java/com/example/foodmates/Activities/PostDetailsActivity.java
@@ -1,27 +1,45 @@
 package com.example.foodmates.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.text.HtmlCompat;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.foodmates.Models.Food;
+import com.example.foodmates.Models.FoodDetail;
 import com.example.foodmates.Models.Post;
 import com.example.foodmates.R;
+import com.parse.ParseException;
+import com.parse.SaveCallback;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.parceler.Parcels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
 
 public class PostDetailsActivity extends AppCompatActivity {
 
+    public static final String API_URL = "https://api.spoonacular.com/recipes/%s/information?apiKey=8cf7ac7ac6f449e49a93e9cf5576c873";
     public static final String TAG = "PostDetailsActivity";
     Post post;
-    ImageView  foodImage;
+    FoodDetail result;
+    TextView postDetail;
+    ImageView foodImage;
     TextView foodTitle;
-    TextView carbs;
-    TextView fat;
-    TextView protein;
-    TextView calories;
     TextView usernameDetail;
     TextView createdAtDetail;
 
@@ -29,35 +47,77 @@ public class PostDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_details);
-        foodImage = (ImageView)  findViewById(R.id.foodDetailImage);
+        foodImage = (ImageView) findViewById(R.id.foodDetailImage);
         foodTitle = (TextView) findViewById(R.id.foodDetailTitle);
-        carbs = (TextView) findViewById(R.id.carbsDetail);
-        fat = (TextView) findViewById(R.id.fatDetail);
-        protein = (TextView) findViewById(R.id.proteinDetail);
-        calories = (TextView) findViewById(R.id.caloriesDetail);
         usernameDetail = (TextView) findViewById(R.id.usernameDetail);
-        createdAtDetail =(TextView) findViewById(R.id.createdAtDetail);
+        createdAtDetail = (TextView) findViewById(R.id.createdAtDetail);
+        postDetail = (TextView) findViewById(R.id.postDetail);
 
 
         post = (Post) Parcels.unwrap(getIntent().getParcelableExtra(Post.class.getSimpleName()));
 
-        if(post.getImageUrl() instanceof String){
+        if (post.getImageUrl() instanceof String) {
             Glide.with(this).load((post.getImageUrl())).into(foodImage);
-        }
-        else {
+        } else {
             Glide.with(this).load(post.getImage().getUrl()).into(foodImage);
         }
 
+
         foodTitle.setText(post.getKeyTitle());
-        if (post.getUser() != null){
+        postDetail.setText(post.getDetail());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Log.i(TAG, "Post set: " + HtmlCompat.fromHtml(post.getDetail(), HtmlCompat.FROM_HTML_MODE_LEGACY));
+            postDetail.setText(HtmlCompat.fromHtml(post.getDetail(), HtmlCompat.FROM_HTML_MODE_LEGACY));
+        } else {
+            postDetail.setText(Html.fromHtml(post.getDetail()));
+        }
+        if (post.getUser() != null) {
             usernameDetail.setText(post.getUser().getUsername());
             createdAtDetail.setText(post.calculateTimeAgo());
         }
-//TODO
-//        carbs.setText(post.getCarbs());
-//        fat.setText(post.getFat());
-//        protein.setText(post.getProtein());
-//        calories.setText(post.getCalories());
+        apiCall();
+
 
     }
-}
+
+    public void apiCall() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = String.format(API_URL,post.getId());
+
+        client.get(url, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess: " + json.toString());
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    Log.i(TAG, "Results: " + jsonObject.toString());
+                    result = FoodDetail.fromJsonObject(jsonObject);
+                    saveInDataBase();
+                } catch (JSONException e) {
+                    Log.e("HomeFragment", "Error Storing api data: " + e.toString());
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG, "Unable to load api", throwable);
+            }
+        });
+
+    }
+
+    private void saveInDataBase() {
+            post.setDetail(result.getDetail());
+            post.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e == null){
+                        Toast.makeText(getBaseContext(), "Successfully saved api result to database", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Log.e(TAG,"Failed to save api result to database");
+                        Log.e(TAG,e.toString());
+                    }
+                }
+            });
+        }
+    }
---
+++ app/src/main/java/com/example/foodmates/Models/FoodDetail.java
@@ -0,0 +1,32 @@
+package com.example.foodmates.Models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Parcel
+public class FoodDetail {
+
+    public String summary;
+
+    public FoodDetail()  {
+    }
+
+
+    public FoodDetail(JSONObject jsonObject) throws JSONException  {
+        summary = jsonObject.getString("summary");
+    }
+
+    public static FoodDetail fromJsonObject(JSONObject object) throws JSONException {
+        return new FoodDetail(object);
+    }
+
+    public String getDetail() {
+        return summary;
+    }
+
+}
