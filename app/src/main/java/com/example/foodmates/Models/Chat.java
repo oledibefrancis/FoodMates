76a54f589988ceea622025be2d9a9c1e0a0f8a45
oledibefrancis <somtofrancis380@gmail.com>
2022-08-02 12:13:23 -0700

    Added Image to my chat model

--- app/src/main/java/com/example/foodmates/Models/Chat.java
+++ app/src/main/java/com/example/foodmates/Models/Chat.java
@@ -1,13 +1,18 @@
 package com.example.foodmates.Models;
 
+import androidx.annotation.Nullable;
+
 import com.parse.ParseClassName;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 
 @ParseClassName("Chat")
-public class Chat extends ParseObject{
+public class Chat extends ParseObject {
 
 
     public static final String KEY_GROUP_NAME = "groupName";
+    private static final String KEY_IMAGE = "Image";
+
 
     public String getGroupName() {
         return getString(KEY_GROUP_NAME);
@@ -16,4 +21,13 @@ public class Chat extends ParseObject{
     public void setGroupName(String groupName) {
         put(KEY_GROUP_NAME, groupName);
     }
+
+    public ParseFile getImage() {
+        return getParseFile(KEY_IMAGE);
+    }
+
+    public void setImage(ParseFile parseFile) {
+        put(KEY_IMAGE, parseFile);
+    }
+
 }
