fb607c1c93560ca6691f9eace8975ea3d1be6a6f
oledibefrancis <somtofrancis380@gmail.com>
2022-08-02 12:12:54 -0700

    Added join and open button functionalities

--- app/src/main/java/com/example/foodmates/Adapters/GroupChatAdapter.java
+++ app/src/main/java/com/example/foodmates/Adapters/GroupChatAdapter.java
@@ -2,9 +2,11 @@ package com.example.foodmates.Adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,11 +16,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.foodmates.Activities.ChatActivity;
 import com.example.foodmates.Models.Chat;
+import com.example.foodmates.Models.Post;
 import com.example.foodmates.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import org.parceler.Parcels;
 
 import java.util.List;
+import java.util.Objects;
 
 public class GroupChatAdapter extends RecyclerView.Adapter<GroupChatAdapter.ViewHolder> {
     Context context;
@@ -40,14 +50,6 @@ public class GroupChatAdapter extends RecyclerView.Adapter<GroupChatAdapter.View
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Chat chat = chats.get(position);
         holder.bind(chat);
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, ChatActivity.class);
-                intent.putExtra(Chat.class.getSimpleName(), Parcels.wrap(chat));
-                context.startActivity(intent);
-            }
-        });
     }
 
     @Override
@@ -58,11 +60,16 @@ public class GroupChatAdapter extends RecyclerView.Adapter<GroupChatAdapter.View
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView groupName;
         ImageView groupImage;
+        Button joinGroup;
+        Button openGroup;
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             groupName = itemView.findViewById(R.id.groupName);
             groupImage = itemView.findViewById(R.id.groupImage);
+            joinGroup = itemView.findViewById(R.id.joinGroup);
+            openGroup = itemView.findViewById(R.id.openGroup);
         }
 
         public void bind(Chat chat) {
@@ -70,6 +77,52 @@ public class GroupChatAdapter extends RecyclerView.Adapter<GroupChatAdapter.View
             Glide.with(context)
                     .load(chat.getImage().getUrl())
                     .into(groupImage);
+
+            ParseUser user = ParseUser.getCurrentUser();
+            ParseRelation<Chat > relation = user.getRelation("userGroups");
+            ParseQuery<Chat> query = relation.getQuery();
+            query.findInBackground(new FindCallback<Chat>() {
+                @Override
+                public void done(List<Chat> groups, ParseException e) {
+                    for(Chat g : groups){
+                        if (Objects.equals(g.getObjectId(), chat.getObjectId())){
+                            openGroup.setVisibility(View.VISIBLE);
+                            joinGroup.setVisibility(View.INVISIBLE);
+                            return;
+                        }
+                        else {
+                            joinGroup.setVisibility(View.VISIBLE);
+                            openGroup.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                    chats.addAll(groups);
+
+                }
+            });
+
+            joinGroup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, ChatActivity.class);
+                    intent.putExtra(Chat.class.getSimpleName(), Parcels.wrap(chat));
+                    relation.add(chat);
+                    user.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                        }
+                    });
+                    context.startActivity(intent);
+                }
+            });
+
+            openGroup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, ChatActivity.class);
+                    intent.putExtra(Chat.class.getSimpleName(), Parcels.wrap(chat));
+                    context.startActivity(intent);
+                }
+            });
         }
     }
 }
